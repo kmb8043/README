cfd869035383e93741887ac7a35f406d508d787a
kmb8043 <you@example.com>
2024-01-04 14:59:51 -0500

    updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input 
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+       
+        {
+            type:'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+    
+        {
+            type:'input',
+            name: 'description',
+            message: 'Add a description about your project.',
+        },
+
+        {
+            type:'input',
+            name: 'screenshot',
+            message: 'Please add a screenshot of your project.',
+        },
+
+        {
+            type:'input',
+            name: 'link',
+            message: 'Please paste your URL link.',
+        },
+
+        {
+            type:'input',
+            name: 'install',
+            message: 'Add installation information for you project'
+        },
+    
+        {
+            type:'input',
+            name: 'usage',
+            message: 'State the usage of your project.',
+        },
+    
+        {
+            type:'input',
+            name: 'GitHub',
+            message: 'Add your GitHub username.',
+        },
+
+        {
+            type:'input',
+            name: 'contributors',
+            message: 'List any contributors. If none, please write "none.',
+        },
+    
+        {
+            type:'list',
+            name: 'license',
+            message: 'Choose a license for the application',
+            choices: ['MIT' , 'Apache' , 'GPL' , 'None' ]
+        },
+]
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+     console.log("Creating a README.md file...");
+     fs.writeToFile("./README.md" , generateMarkdown(answers));
+
+    });
+   }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)'
+  }
+  if(license === 'Apache'){
+    return '![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)'
+  }
+  if(license === 'GPL'){
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if(license === 'None'){
+    return 'None'
+  }
+  }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `/* Copyright (c) [year] [fullname] */`;
+}
+return "";
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+## Description 
+${data.description}
+
+## Screenshot
+${data.screenshot}
+
+
+## Link to deployed application URL
+${data.link}
+
+## GitHub
+${dara.Github}
+
+## Table of Contents
+  * Installation
+  * Usage
+  * GitHub
+  * Contributors
+  * Lisence
+
+## Installation
+To install the application, follow these steps:
+
+${data.install}
+
+## How to use this application
+${data.usage}
+
+## Contributors
+${data.contributors}
+
+## License
+${renderLicenseBadge(data.license)}
+${renderLicenseLink(data.license)}
+${renderLicenseLink(data.license)}
+
+`};
 
 module.exports = generateMarkdown;
